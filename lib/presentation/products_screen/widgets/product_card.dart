59079495742a40d464ffed075ea2ec4dadb6b7f1
Alex R <alexregianideveloper@outlook.com>
2023-01-27 15:52:43 -0400

    added listile title, subtitle to card

--- lib/presentation/products_screen/widgets/product_card.dart
+++ lib/presentation/products_screen/widgets/product_card.dart
@@ -5,35 +5,47 @@ class ProductCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(borderRadius: BorderRadius.circular(90)),
-      width: 200,
-      height: 300,
-      child: Card(
-        clipBehavior: Clip.antiAliasWithSaveLayer,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(20),
-        ),
-        elevation: 10,
-        child: Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: FittedBox(
-            alignment: Alignment.topCenter,
-            fit: BoxFit.contain,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(50),
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(500),
-                ),
-                child: Image.asset(
-                  'assets/chair1.jpeg',
+    return Stack(children: [
+      Container(
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(90)),
+        width: 200,
+        height: 300,
+        child: Card(
+          // clipBehavior: Clip.antiAliasWithSaveLayer,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          elevation: 10,
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: FittedBox(
+              alignment: Alignment.topCenter,
+              fit: BoxFit.contain,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(50),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                  child: Image.asset(
+                    'assets/chair1.jpeg',
+                  ),
                 ),
               ),
             ),
           ),
         ),
       ),
-    );
+      const Positioned(
+        bottom: 30,
+        top: 175,
+        left: 5,
+        width: 150,
+        child: ListTile(
+          title: Text('Green Chair'),
+          subtitle: Text('Description'),
+        ),
+      ),
+    ]);
   }
 }
