d9fe198dc8ccb114fdff1978f8fc2b12cd1e05b9
Alex R <alexregianideveloper@outlook.com>
2023-01-30 11:40:12 -0400

    added navigation

--- lib/presentation/products_screen/widgets/product_card_info.dart
+++ lib/presentation/products_screen/widgets/product_card_info.dart
@@ -7,7 +7,23 @@ class ProductCardInfo extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Column(children: [Image.asset('assets/chair1.jpeg')]),
+      body: Column(children: [
+        ClipRRect(
+            borderRadius: const BorderRadius.only(
+              bottomLeft: Radius.circular(40),
+              bottomRight: Radius.circular(40),
+            ),
+            child: Image.asset('assets/chair1.jpeg')),
+        ListTile(
+          title: Text('500.0'),
+          subtitle: Text('Chair'),
+        ),
+        ListTile(
+          title: Text('Description'),
+          subtitle: Text(
+              '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."'),
+        )
+      ]),
     );
   }
 }
