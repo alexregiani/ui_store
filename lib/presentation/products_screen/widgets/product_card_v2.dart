5e177955d0e30f75306f7840f0a5223c3a29d014
Alex R <alexregianideveloper@outlook.com>
2023-01-30 14:36:28 -0400

    new card design with better dynamics

--- lib/presentation/products_screen/widgets/product_card_v2.dart
+++ lib/presentation/products_screen/widgets/product_card_v2.dart
@@ -17,58 +17,58 @@ class ProductCardV2 extends StatelessWidget {
     return Container(
       width: 200,
       height: 300,
-      child: InkWell(
-        onTap: () => Navigator.pushNamed(context, '/ProductCardInfo'),
-        child: Card(
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-          child: Stack(
-            children: [
-              Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(12.0),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(20),
-                      // borderRadius: BorderRadius.circular(60),
+      child: Card(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        child: Stack(
+          children: [
+            Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    // borderRadius: BorderRadius.circular(60),
+                    child: InkWell(
+                      onTap: () =>
+                          Navigator.pushNamed(context, '/ProductCardInfo'),
                       child: Image.asset(
                         'assets/chair1.jpeg',
                       ),
                     ),
                   ),
-                  Expanded(
-                    child: ListTile(
-                      title: Text(title),
-                      subtitle: Text(subtitle),
-                    ),
+                ),
+                Expanded(
+                  child: ListTile(
+                    title: Text(title),
+                    subtitle: Text(subtitle),
                   ),
-                  ListTile(
-                    title: Text('$price'),
+                ),
+                ListTile(
+                  title: Text('$price'),
+                ),
+              ],
+            ),
+            Align(
+              alignment: Alignment.bottomRight,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Ink(
+                  height: 40,
+                  width: 40,
+                  decoration: const ShapeDecoration(
+                    shape: CircleBorder(),
+                    color: Colors.blue,
                   ),
-                ],
-              ),
-              Align(
-                alignment: Alignment.bottomRight,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Ink(
-                    height: 40,
-                    width: 40,
-                    decoration: const ShapeDecoration(
-                      shape: CircleBorder(),
-                      color: Colors.blue,
-                    ),
-                    child: const IconButton(
-                      onPressed: null,
-                      icon: Icon(
-                        Icons.add,
-                      ),
+                  child: const IconButton(
+                    onPressed: null,
+                    icon: Icon(
+                      Icons.add,
                     ),
                   ),
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
