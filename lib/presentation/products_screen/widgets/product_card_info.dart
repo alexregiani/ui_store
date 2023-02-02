384403d32a1957e9b8c207f860823714bd274ed9
Alex R <alexregianideveloper@outlook.com>
2023-02-02 16:09:05 -0400

    changes

--- lib/presentation/products_screen/widgets/product_card_info.dart
+++ lib/presentation/products_screen/widgets/product_card_info.dart
@@ -13,32 +13,24 @@ class ProductCardInfo extends StatelessWidget {
         backgroundColor: Colors.transparent,
         elevation: 0,
       ),
-      body: Column(children: [
-        Stack(children: [
-          ClipRRect(
-            borderRadius: const BorderRadius.only(
-              bottomLeft: Radius.circular(40),
-              bottomRight: Radius.circular(40),
-            ),
-            child: Image.asset('assets/chair1.jpeg'),
+      body: Stack(children: [
+        ClipRRect(
+          borderRadius: const BorderRadius.only(
+            bottomLeft: Radius.circular(40),
+            bottomRight: Radius.circular(40),
           ),
-          ElevatedButton(
+          child: Image.asset('assets/chair1.jpeg'),
+        ),
+        Align(
+          alignment: Alignment.bottomRight,
+          child: ElevatedButton(
               style: ElevatedButton.styleFrom(
                   backgroundColor: Colors.blue,
                   fixedSize: Size(50, 50),
                   shape: CircleBorder(),
                   elevation: 5),
               onPressed: () => print('pressed'),
-              child: Icon(Icons.ac_unit_rounded))
-        ]),
-        const ListTile(
-          title: Text('Chair', style: TextStyle(fontSize: 25)),
-          subtitle: Text('500.0', style: TextStyle(fontSize: 20)),
-        ),
-        const ListTile(
-          title: Text('Description'),
-          subtitle: Text(
-              '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."'),
+              child: Icon(Icons.ac_unit_rounded)),
         )
       ]),
     );
